f37acb795bc14080bef1ffa5669a2781743e25a2
Denis Verkhoturov <denis_verkhoturov@epam.com>
2018-04-10 07:19:36 +0300

    feat: Add accommodation model

---
+++ src/main/kotlin/com/github/octoacademy/sedona/models/Accommodation.kt
@@ -0,0 +1,17 @@
+package com.github.octoacademy.sedona.models
+
+enum class StarRating {
+    ONE, TWO, THREE, FOUR, FIVE
+}
+
+enum class Type {
+    HOTEL, MOTEL, APARTMENTS
+}
+
+data class Accommodation(
+        val name: String,
+        val type: Type,
+        val price: Int,
+        val stars: StarRating,
+        val rating: Double
+)
