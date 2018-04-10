c4b7e47d7e0ae182f7e023f3598d971646d4b71a
Denis Verkhoturov <denis_verkhoturov@epam.com>
2018-04-10 07:18:08 +0300

    feat: Add application entry point

---
+++ src/main/kotlin/com/github/octoacademy/sedona/Application.kt
@@ -0,0 +1,11 @@
+package com.github.octoacademy.sedona
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
