12f326ac7abb4df576cab7e76092090dbd38a2ea
Denis Verkhoturov <denis_verkhoturov@epam.com>
2018-04-19 15:15:19 +0300

    feat: LodgingsController delegates logic to LodgingService

--- src/main/kotlin/com/github/octoacademy/sedona/controllers/LodgingController.kt
+++ src/main/kotlin/com/github/octoacademy/sedona/controllers/LodgingController.kt
@@ -1,8 +1,7 @@
 package com.github.octoacademy.sedona.controllers
 
 import com.github.octoacademy.sedona.models.Lodging
-import com.github.octoacademy.sedona.models.StarRating
-import com.github.octoacademy.sedona.models.Type
+import com.github.octoacademy.sedona.services.LodgingService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -17,39 +16,32 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/lodgings")
 class LodgingController {
-    val lodgings = mutableListOf(
-            Lodging("Amara-Resort & SPA", Type.HOTEL, 4000, StarRating.FOUR, 8.5),
-            Lodging("Desert Quail Inn", Type.MOTEL, 3000, StarRating.THREE, 8.9),
-            Lodging("Vila at Poco Diablo", Type.APARTMENTS, 2000, StarRating.TWO, 9.2)
-    )
 
     @GetMapping
     fun list() : List<Lodging> {
-        return lodgings
+        return LodgingService.list()
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun create(@RequestBody lodging : Lodging) : Lodging {
-        lodgings.add(lodging)
-        return lodging
+        return LodgingService.create(lodging)
     }
 
     @GetMapping("/{id}")
     @Throws(RuntimeException::class)
     fun read(@PathVariable id : Int) : Lodging? {
-        return if (lodgings.size > id) lodgings[id] else null
+        return LodgingService.read(id)
     }
 
     @PutMapping("/{id}")
     fun update(@PathVariable id : Int, @RequestBody lodging: Lodging) : Lodging {
-        lodgings[id] = lodging
-        return lodging
+        return LodgingService.update(id, lodging)
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id : Int) {
-        lodgings.removeAt(id)
+        LodgingService.remove(id)
     }
 }
