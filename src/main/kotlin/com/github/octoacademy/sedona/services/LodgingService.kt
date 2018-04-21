127d680d36ea3022d7e3ba09fb69c5f7926d222e
Denis Verkhoturov <denis_verkhoturov@epam.com>
2018-04-21 16:36:22 +0300

    feat: Replace hardcoded entities based logic with proxying to repository

--- src/main/kotlin/com/github/octoacademy/sedona/services/LodgingService.kt
+++ src/main/kotlin/com/github/octoacademy/sedona/services/LodgingService.kt
@@ -1,31 +1,20 @@
 package com.github.octoacademy.sedona.services
 
 import com.github.octoacademy.sedona.models.Lodging
-import com.github.octoacademy.sedona.models.StarRating
-import com.github.octoacademy.sedona.models.Type
+import com.github.octoacademy.sedona.repositories.LodgingRepository
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Service
 
 @Service
 object LodgingService {
-    private val lodgings = mutableListOf(
-            Lodging("Amara-Resort & SPA", Type.HOTEL, 4000, StarRating.FOUR, 8.5),
-            Lodging("Desert Quail Inn", Type.MOTEL, 3000, StarRating.THREE, 8.9),
-            Lodging("Vila at Poco Diablo", Type.APARTMENTS, 2000, StarRating.TWO, 9.2)
-    )
 
-    fun list(): List<Lodging> = lodgings
+    fun list(): List<Lodging> = transaction { LodgingRepository.list() }
 
-    fun create(lodging: Lodging): Lodging {
-        lodgings.add(lodging)
-        return lodging
-    }
+    fun create(lodging: Lodging): Lodging = transaction { LodgingRepository.create(lodging) }
 
-    fun read(id : Int): Lodging? = if (lodgings.size > id) lodgings[id] else null
+    fun read(id: Int): Lodging? = transaction { LodgingRepository.read(id) }
 
-    fun update(id: Int, lodging: Lodging): Lodging {
-        lodgings[id] = lodging
-        return lodging
-    }
+    fun update(id: Int, lodging: Lodging): Lodging = transaction { LodgingRepository.update(id, lodging) }
 
-    fun remove(id: Int) = lodgings.removeAt(id)
+    fun remove(id: Int) = transaction { LodgingRepository.remove(id) }
 }
