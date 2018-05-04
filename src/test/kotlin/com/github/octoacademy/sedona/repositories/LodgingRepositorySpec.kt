f9d97bcd7cdd4a0f4263e30cf59831829fd1fb64
Denis Verkhoturov <denis_verkhoturov@epam.com>
2018-05-04 13:04:15 +0300

    refactor: Rewrite LodgingRepositorySpec using conventional BDD approach

--- src/test/kotlin/com/github/octoacademy/sedona/repositories/LodgingRepositorySpec.kt
+++ src/test/kotlin/com/github/octoacademy/sedona/repositories/LodgingRepositorySpec.kt
@@ -5,6 +5,7 @@ import com.github.octoacademy.sedona.models.Type
 import com.github.octoacademy.sedona.schemas.Lodgings
 import org.amshove.kluent.`should be in`
 import org.amshove.kluent.`should be`
+import org.amshove.kluent.`should contain all`
 import org.amshove.kluent.`should equal`
 import org.amshove.kluent.`should not be in`
 import org.amshove.kluent.`should throw`
@@ -12,105 +13,208 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.dsl.ActionBody
+import org.jetbrains.spek.api.dsl.SpecBody
+import org.jetbrains.spek.api.dsl.TestBody
+import org.jetbrains.spek.api.dsl.TestContainer
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it as originalIt
+import org.jetbrains.spek.api.dsl.on as originalOn
 import java.sql.SQLException
 
 class LodgingRepositorySpec : Spek({
 
-    describe("LodgingRepository") {
-        val lodgingRepository = LodgingRepository()
+    val lodgingRepository = LodgingRepository()
 
-        val hotelCalifornia = Lodging("Hotel California", Type.HOTEL, 999, 3, 7.8)
-        val grandBudapest = Lodging("Grand Budapest", Type.APARTMENTS, 5000, 5, 9.8)
+    val hotelCalifornia = Lodging("Hotel California", Type.HOTEL, 999, 3, 7.8)
+    val grandBudapest = Lodging("Grand Budapest", Type.APARTMENTS, 5000, 5, 9.8)
+    val mariposaSaloon = Lodging("Mariposa Saloon", Type.MOTEL, 5000, 5, 8.9)
 
-        val invalidLodging = Lodging("", Type.MOTEL, -42, 42, 42.42)
+    val invalidLodging = Lodging("", Type.MOTEL, -42, 42, 42.42)
+
+    /**
+     * Creates an [action][SpecBody.action] where body wrapped with transaction.
+     */
+    fun SpecBody.on(description: String, body: ActionBody.() -> Unit) {
+        originalOn(description) { transaction { body() } }
+    }
+
+    /**
+     * Creates a [test][SpecBody.test] where body wrapped with transaction.
+     */
+    fun TestContainer.it(description: String, body: TestBody.() -> Unit) {
+        originalIt(description) { transaction { body() } }
+    }
+
+    given("a lodging repository") {
 
         beforeGroup {
             val db = createTempFile("sedona-backend", ".db").apply { deleteOnExit() }
             Database.connect(url = "jdbc:h2:${db.absolutePath}", driver = "org.h2.Driver")
         }
 
-        beforeEachTest { transaction {
-            SchemaUtils.drop(Lodgings)
-            SchemaUtils.create(Lodgings)
-        } }
+        beforeEachTest {
+            transaction {
+                SchemaUtils.drop(Lodgings)
+                SchemaUtils.create(Lodgings)
+            }
+        }
+
+        on("attempting to create invalid lodging") {
+            val attemptToCreate = { lodgingRepository.create(invalidLodging) }
+
+            it("should throw exception") {
+                attemptToCreate `should throw` SQLException::class
+            }
+
+            it("should not appear in the lodgings list") {
+                invalidLodging `should not be in` lodgingRepository.list()
+            }
+        }
+
+        on("attempting to create lodging if one already exists") {
+            lodgingRepository.create(hotelCalifornia)
+            val attemptToCreate = { lodgingRepository.create(hotelCalifornia) }
+
+            it("should throw exception") {
+                attemptToCreate `should throw` SQLException::class
+            }
+
+            it("should not double lodgings") {
+                lodgingRepository.list() `should equal` listOf(hotelCalifornia)
+            }
+        }
 
         on("creating new lodging") {
-            it("should throw exception when lodging object is invalid") { transaction {
-                val attemptToCreate = { lodgingRepository.create(invalidLodging) }
-                attemptToCreate `should throw` SQLException::class
-                lodgingRepository.list() `should equal` emptyList()
-            } }
+            val id = lodgingRepository.create(hotelCalifornia)
 
-            it("should return id of created lodging when lodging object is valid") { transaction {
-                val id = lodgingRepository.create(hotelCalifornia)
+            it("should return id of created lodging") {
                 lodgingRepository.read(id) `should equal` hotelCalifornia
-            } }
+            }
+
+            it("should appear in the lodgings list") {
+                hotelCalifornia `should be in` lodgingRepository.list()
+            }
+        }
+
+        on("attempting to read lodging with id that doesn't exist") {
+            val hotelCaliforniaId = lodgingRepository.create(hotelCalifornia)
+            val grandBudapestId = lodgingRepository.create(grandBudapest)
+            val nonExistentId = hotelCaliforniaId + grandBudapestId
+
+            it("should return null") {
+                lodgingRepository.read(nonExistentId) `should be` null
+            }
         }
 
         on("reading lodging") {
-            it("should return null when lodging with given id doesn't exist") { transaction {
-                lodgingRepository.read(1) `should be` null
-            } }
+            val id = lodgingRepository.create(hotelCalifornia)
 
-            it("should return lodging when lodging with given id exists") { transaction {
-                val id = lodgingRepository.create(hotelCalifornia)
+            it("should return lodging") {
                 lodgingRepository.read(id) `should equal` hotelCalifornia
-            } }
+            }
+        }
+
+        on("attempting to update lodging with id that doesn't exist") {
+            val hotelCaliforniaId = lodgingRepository.create(hotelCalifornia)
+            val grandBudapestId = lodgingRepository.create(grandBudapest)
+            val nonExistentId = hotelCaliforniaId + grandBudapestId
+            val updated = lodgingRepository.update(nonExistentId, mariposaSaloon)
+
+            it("should return false") {
+                updated `should be` false
+            }
+
+            it("should not affect existing lodgings") {
+                lodgingRepository.list() `should contain all` listOf(hotelCalifornia, grandBudapest)
+            }
+
+            it("should not appear in the lodging list") {
+                mariposaSaloon `should not be in` lodgingRepository.list()
+            }
+        }
+
+        on("attempting to update lodging with invalid one") {
+            val hotelCaliforniaId = lodgingRepository.create(hotelCalifornia)
+            lodgingRepository.create(grandBudapest)
+            lodgingRepository.create(mariposaSaloon)
+            val attemptToUpdate = { lodgingRepository.update(hotelCaliforniaId, invalidLodging) }
+
+            it("should throw exception") {
+                attemptToUpdate `should throw` SQLException::class
+            }
+
+            it("should not affect existing lodgings") {
+                lodgingRepository.list() `should contain all` listOf(hotelCalifornia, grandBudapest, mariposaSaloon)
+            }
+
+            it("should not appear in the lodging list") {
+                invalidLodging `should not be in` lodgingRepository.list()
+            }
         }
 
         on("updating lodging") {
-            val id = transaction { lodgingRepository.create(hotelCalifornia) }
+            val id = lodgingRepository.create(hotelCalifornia)
+            val updated = lodgingRepository.update(id, grandBudapest)
 
-            it("should return false when lodging with given id doesn't exist") { transaction {
-                lodgingRepository.update(42, hotelCalifornia) `should be` false
-            } }
+            it("should return true") {
+                updated `should be` true
+            }
 
-            it("should return true when lodging with given id exists") { transaction {
-                lodgingRepository.update(id, grandBudapest) `should equal` true
-            } }
+            it("should provide new lodging on reading") {
+                lodgingRepository.read(id) `should equal` grandBudapest
+            }
+        }
 
-            it("should replace existent lodging with new one when given id exists") { transaction {
-                lodgingRepository.update(id, grandBudapest)
-                val allTheLodgings = lodgingRepository.list()
-                grandBudapest `should be in` allTheLodgings
-                hotelCalifornia `should not be in` allTheLodgings
-            } }
+        on("attempting to remove lodging with id that doesn't exist") {
+            val hotelCaliforniaId = lodgingRepository.create(hotelCalifornia)
+            val grandBudapestId = lodgingRepository.create(grandBudapest)
+            val mariposaSaloonId = lodgingRepository.create(mariposaSaloon)
+            val nonExistentId = hotelCaliforniaId + grandBudapestId + mariposaSaloonId
+            val removed = lodgingRepository.remove(nonExistentId)
+
+            it("should return false") {
+                removed `should be` false
+            }
+
+            it("should not affect existing lodgings") {
+                lodgingRepository.list() `should contain all` listOf(hotelCalifornia, grandBudapest, mariposaSaloon)
+            }
         }
 
         on("removing lodging") {
-            it("should return false when such lodging didn't found") { transaction {
-                lodgingRepository.remove(1) `should be` false
-            } }
+            val id = lodgingRepository.create(hotelCalifornia)
+            lodgingRepository.create(grandBudapest)
+            lodgingRepository.create(mariposaSaloon)
+            val removed = lodgingRepository.remove(id)
 
-            it("should return true when such lodging was found") { transaction {
-                val id = lodgingRepository.create(hotelCalifornia)
-                hotelCalifornia `should be in` lodgingRepository.list()
-                lodgingRepository.remove(id) `should be` true
-            } }
+            it("should return true") {
+                removed `should be` true
+            }
 
-            it("should remove lodging when one exists") { transaction {
-                val id = lodgingRepository.create(grandBudapest)
-                grandBudapest `should be in` lodgingRepository.list()
-                lodgingRepository.remove(id)
+            it("should remove lodging under given id") {
                 hotelCalifornia `should not be in` lodgingRepository.list()
-            } }
+            }
+
+            it("should not affect other lodgings") {
+                lodgingRepository.list() `should contain all` listOf(grandBudapest, mariposaSaloon)
+            }
         }
 
-        on("listing all lodgings") {
-            it("should return an empty list when there are no lodgings yet") { transaction {
+        on("listing all lodgings when there are no lodgings yet") {
+            it("should return an empty list") {
                 lodgingRepository.list() `should equal` emptyList()
-            } }
+            }
+        }
 
-            it("should return all the lodgings when there are lodgings exists") { transaction {
-                lodgingRepository.create(hotelCalifornia)
-                lodgingRepository.create(grandBudapest)
-                val allTheLodgings = lodgingRepository.list()
-                hotelCalifornia `should be in` allTheLodgings
-                grandBudapest `should be in` allTheLodgings
-            } }
+        on("listing all lodgings when there are lodgings exist") {
+            lodgingRepository.create(hotelCalifornia)
+            lodgingRepository.create(grandBudapest)
+            lodgingRepository.create(mariposaSaloon)
+
+            it("should return list of all the lodgings") {
+                lodgingRepository.list() `should contain all` listOf(hotelCalifornia, grandBudapest, mariposaSaloon)
+            }
         }
     }
 })
